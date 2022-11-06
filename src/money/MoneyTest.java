2a3f762544ce961313f5d9da2954b696fd483af4
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-11-06 17:50:17 +0900

    feat(money): impl times

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -80,6 +80,27 @@ public class MoneyTest {
     bank.addRate("CHF", "USD", 2);
     Money result = bank.reduce(fiveBucks.plus(tenFranc), "USD");
     assertEquals(Money.dollar(10), result);
+  }
 
+  @Test
+  public void testSumPlusMoney() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFranc = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFranc).plus(fiveBucks);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(15), result);
+  }
+
+  @Test
+  public void testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFranc = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFranc).times(2);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(20), result);
   }
 }
